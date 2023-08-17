4ee15ea205827e9ae8724c0b2e831dac9bb2fd9e
pedrohnrqfreitas <valtpedrointens@gmail.com>
2023-08-17 12:05:56 -0300

    ProjectFinished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'telinha5';
+
+  numTenis:number=0
+  abreEfecha:boolean=false
+
+
+
+  actualImage: string = 'assets/image-product-1.jpg'
+
+  public contadorMais(){
+    this.numTenis = this.numTenis + 1;
+  }
+  public contadorMenos(){
+    this.numTenis = this.numTenis - 1;
+  }
+  public verifica(){
+    if(this.numTenis < 0 ){
+      return this.numTenis=0
+    }else{
+      return this.numTenis
+    }
+}
 }
